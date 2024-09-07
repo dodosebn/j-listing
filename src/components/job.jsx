ed65992e22109c696cdf6c89ba0ddf4d84de2a31
Orji Dominion Ebubennia <orjidominion32@gmail.com>
2024-09-07 20:35:00 +0100

    first

--- src/components/job.jsx
+++ src/components/job.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import svgMap from './svgImports'; 
-import '../job.css';
+import React from "react";
+import svgMap from "./svgImports";
+import "../job.css";
 
 const Job = (props) => {
   const {
@@ -19,38 +19,38 @@ const Job = (props) => {
   } = props.data;
 
   let keywords = [role, level, ...languages, ...tools];
-  const iconName = logo.split('/').pop(); // Extracts the filename from the path
-  const icon = svgMap[iconName] || ''; 
+  const iconName = logo.split("/").pop(); 
+  const icon = svgMap[iconName] || "";
 
   return (
     <div className="job-container">
       <div className="job-holder">
-      <div className="logo">
-        <img src={icon} alt={`Logo for ${company}`} />
-      </div>
+        <div className="logo">
+          <img src={icon} alt={`Logo for ${company}`} />
+        </div>
 
-      <div className="part1">
-        <div className="company">
-          <div className="cname">{company}</div>
-          {isNew && <span className='new'>new!</span>}
-          {featured && <span className='featured'>featured</span>}
+        <div className="part1">
+          <div className="company">
+            <div className="cname">{company}</div>
+            {isNew && <span className="new">new!</span>}
+            {featured && <span className="featured">featured</span>}
+          </div>
+          <div className="position">{position}</div>
+          <div className="details">
+            <span>{postedAt}</span>
+            <span>&nbsp;&nbsp;•&nbsp;&nbsp;</span>
+            <span>{contract}</span>
+            <span>&nbsp;&nbsp;•&nbsp;&nbsp;</span>
+            <span>{location}</span>
+          </div>
         </div>
-        <div className="position">{position}</div>
-        <div className="details">
-          <span>{postedAt}</span>
-          <span>&nbsp;&nbsp;•&nbsp;&nbsp;</span>
-          <span>{contract}</span>
-          <span>&nbsp;&nbsp;•&nbsp;&nbsp;</span>
-          <span>{location}</span>
+        <div className="part2">
+          {keywords.map((key, index) => (
+            <button onClick={() => props.setKeywords(key)} key={index}>
+              {key}
+            </button>
+          ))}
         </div>
-      </div>
-      <div className="part2">
-        {keywords.map((key, index) => (
-          <button onClick={() => props.setKeywords(key)} key={index}>
-            {key}
-          </button>
-        ))}
-      </div>
       </div>
     </div>
   );
--- src/components/jobs.jsx
+++ src/components/jobs.jsx
@@ -1,35 +1,35 @@
-import React, { useEffect, useState, useCallback } from 'react';
-import Job from './job'; 
-import '../job.css';
+import React, { useEffect, useState, useCallback } from "react";
+import Job from "./job";
+import "../job.css";
 
 const Jobs = ({ data, setKeywords, keywords }) => {
   const [filteredData, setFilteredData] = useState(data);
 
-  // Memoize filterData to prevent unnecessary re-creations
   const filterData = useCallback(() => {
     if (keywords.length > 0) {
-      const newData = data.filter(d => 
-        keywords.every(key => 
-          d.role === key ||
-          d.level === key ||
-          d.languages.includes(key) ||
-          d.tools.includes(key)
+      const newData = data.filter((d) =>
+        keywords.every(
+          (key) =>
+            d.role === key ||
+            d.level === key ||
+            d.languages.includes(key) ||
+            d.tools.includes(key)
         )
       );
       setFilteredData(newData);
     } else {
       setFilteredData(data);
     }
-  }, [data, keywords]); // Include data and keywords in the dependency array
+  }, [data, keywords]);
 
-  // Use useEffect to run filterData when keywords or data change
+
   useEffect(() => {
     filterData();
-  }, [filterData]); // Include filterData in the dependency array
+  }, [filterData]); 
 
   return (
     <div className="jobs">
-      {filteredData.map(d => (
+      {filteredData.map((d) => (
         <Job key={d.id} data={d} setKeywords={setKeywords} />
       ))}
     </div>
